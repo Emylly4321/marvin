42ae0239aa4bafdf5a2830a0911ca37b7921a2f6
Emylly4321 <59850938+Emylly4321@users.noreply.github.com>
2020-01-13 21:49:41 -0300

    3 fase

---
+++ fase01/ex02/nomes.js
@@ -0,0 +1,20 @@
+function mostrarNome() {
+    console.log ('marvin')
+}
+
+mostrarNome()
+
+
+function nomeDeVolta(){
+   
+
+}
+
+ console.log ("O androide paranoide");
+ 
+ 
+ function nomes (nome1, nome2){
+ }
+
+ 
+ console.log ('marvin, cachalote')
